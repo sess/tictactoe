92c2aa52c48b7c980efaec812830bbe4f971836d
Kevin <kdou@salesforce.com>
2018-11-15 04:13:00 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+/* The button click handlers here. Put the 
+performLogic function inside each of the buttons */
+
+$("#button1").click( function() {
+});
+
+$("#button2").click( function() {
+});
+
+$("#button3").click( function() {
+});
+
+$("#button4").click( function() {
+});
+
+$("#button5").click( function() {
+});
+
+$("#button6").click( function() {
+});
+
+$("#button7").click( function() {
+});
+
+$("#button8").click( function() {
+});
+
+$("#button9").click( function() {
+});
+
+/* This function takes in two parameters, and will
+be where you write most of the logic of the game */
+function performLogic(buttonId, tileId) {
+
+}
